e002eb4f6fcb67046b81b3d51fa0b40a621fde52
Zahidalwan <zahidalwan1601@gmail.com>
2025-09-09 17:33:31 +0700

    Delete

--- src/handler.js
+++ src/handler.js
@@ -12,6 +12,14 @@ export const getAllBooksHandler = (req, res) => {
 export const addBookHandler = (req, res) => {
   const { name, author } = req.body;
 
+  //validasi
+  if (!name || !name.trim()) {
+    return res.status(400).json({
+      status: "fail",
+      message: "Gagal menambahkan buku. Mohon isi nama buku",
+    });
+  }
+
   const id = Date.now();
   //wadah
   const newBook = { id, name, author };
@@ -68,5 +76,29 @@ export const updateBookByIdHandler = (req, res) => {
     status: "success",
     message: "Yeaaay, Buku berhasil diubah",
     data: { book },
+  });
+};
+
+//menghapus buku berdasarkan ID
+export const deleteBookByIdHandler = (req, res) => {
+  const { bookid } = req.params;
+  const numeric = Number(bookid);
+  const bookIndex = books.findIndex((b) => b.id === numeric);
+
+  const book = books.find((b) => b.id === numeric);
+
+  if (!book) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Maaf, Buku tidak ditemukan",
+    });
+  }
+
+  const index = books.indexOf(book);
+  books.splice(index, 1);
+
+  res.status(200).json({
+    status: "success",
+    message: "Yeaaaay, Buku berhasil dihapus",
   });
 };
